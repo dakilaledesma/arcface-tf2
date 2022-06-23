ef354b2f761353e5621b3b0c6edefded5caab0bd
Dax Ledesma <dakilaledesma@gmail.com>
2022-06-23 13:18:18 -0400

    Accuracy score

--- analyze_results.py
+++ analyze_results.py
@@ -1,18 +1,19 @@
 import os
+from sklearn.metrics import accuracy_score
 result_file = open("arcface_results.csv")
 result_lines = result_file.readlines()
 rename_file = open("rename_results.csv")
 rename_lines = rename_file.readlines()
 
-cat_dict = {v: k for k, v in [z.strip().split(',') for z in rename_lines]}
+cat_dict = {int(v): int(k) for k, v in [z.strip().split(',') for z in rename_lines]}
 
+y_pred = []
+y_true = []
 for line in result_lines:
     line = line.strip().split(',')
-    pred = line[0]
-    cat = line[1].split('/')[-2]
+    pred = int(line[0])
+    cat = int(line[1].split('/')[-2])
+    y_pred.append(cat_dict[pred])
+    y_true.append(cat)
 
-    print(cat_dict[pred], cat)
-
-
-    
-
+print(accuracy_score(y_pred, y_true))
